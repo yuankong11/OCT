10d7f8f65f7bbaab8b6468b303e3a026c20af6fd
Milky2018 <37078809+Milky2018@users.noreply.github.com>
2020-12-10 21:34:52 +0800

    临时加了个路由，防止刷新页面出现错误

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
     root 'frontend#home'
     get 'frontend/*path', to: 'frontend#show'
+
+    # Temporary deal, routing any vue page to dashboard
+    get 'app/*path', to: 'frontend#show'
 end
