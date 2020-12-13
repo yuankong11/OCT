f967f0452d10996b500255ff9daf387f4962b8b2
Yiner <yulianwei1998@163.com>
2020-12-13 16:38:58 +0800

    spider module demo-1

---
+++ app/helpers/courses_spider.rb
@@ -0,0 +1,28 @@
+module CoursesSpider
+    SEP_URL = 'http://sep.ucas.ac.cn'
+    SAKAI_URL = SEP_URL + '/portal/site/16/801'
+    def get_courses
+        @dr.get SEP_URL
+        @dr.get SAKAI_URL
+
+        @dr.find_element(:id, "loginUser").click
+
+        begin
+          @dr.find_element(:xpath, "//*[contains(@href,'anotherUser')]").click
+        rescue Selenium::WebDriver::Error::NoSuchElementError => e
+
+        else
+
+        ensure
+
+        end
+
+        find_data_site_id = /(\d{6})/
+
+        return @dr.find_elements(:class, "site-favorite-btn").map do |elem|
+          elem.attribute("data-site-id")
+        end.select do |site|
+          site =~ find_data_site_id
+        end
+      end
+end
--- app/helpers/frontend_helper.rb
+++ app/helpers/frontend_helper.rb
@@ -80,31 +80,6 @@ module FrontendHelper
       @dr.find_element(:id, "sb").click 
     end 
   
-    def get_courses 
-      @dr.get SEP_URL
-      @dr.get SAKAI_URL 
-  
-      @dr.find_element(:id, "loginUser").click 
-  
-      begin
-        @dr.find_element(:xpath, "//*[contains(@href,'anotherUser')]").click 
-      rescue Selenium::WebDriver::Error::NoSuchElementError => e 
-        
-      else
-        
-      ensure
-        
-      end
-      
-      find_data_site_id = /(\d{6})/ 
-  
-      return @dr.find_elements(:class, "site-favorite-btn").map do |elem|
-        elem.attribute("data-site-id")
-      end.select do |site| 
-        site =~ find_data_site_id
-      end
-    end 
-  
     def img_location_str(img_element)
       location = img_element.location 
       size = img_element.size 
