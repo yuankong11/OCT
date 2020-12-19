7672d35c7f779782a2be97eb16dc07b713a1ca68
Milky2018 <37078809+Milky2018@users.noreply.github.com>
2020-12-19 18:59:15 +0800

    资源获取函数实现

--- app/helpers/resource_spider.rb
+++ app/helpers/resource_spider.rb
@@ -1,3 +1,17 @@
 module ResourceSpider
   include CourseSpider
+
+  RESOURCE_URL_S = "https://course.ucas.ac.cn/access/content/group/"
+
+  def get_resources(course_code)
+    course_link = RESOURCE_URL_S + course_code
+    res = agent.get(course_link)
+    file_infos = res.links.map do |link|
+      {
+        "name" => link.text,
+        "address" => course_link + link.href
+      }
+    end
+    return file_infos
+  end
 end
