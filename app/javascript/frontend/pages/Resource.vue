8a60e48896da46dbf400dff2a3d159928413431a
Milky2018 <37078809+Milky2018@users.noreply.github.com>
2021-01-08 16:44:01 +0800

    资源页面UI调整，现在已经很帅了，但排序、显示文件大小、显示更新时间暂时无法实现

--- app/javascript/frontend/pages/Resource.vue
+++ app/javascript/frontend/pages/Resource.vue
@@ -1,53 +1,64 @@
 <template>
 <v-app>
-  <v-content class="text-left">
-    <v-btn icon @click="refresh()">
-      <v-icon>mdi-cached</v-icon>
-    </v-btn>
-    <v-btn icon @click="handleDownloadZip()">
-      <v-icon>mdi-download</v-icon>
-    </v-btn>
-    <v-btn v-show="downloading">
-      正在下载，请稍后...
-      <v-icon @click="downloading = false" color="green">
-        mdi-close
-      </v-icon>
-    </v-btn>
-    <v-btn v-show="emptytree" color="orange">
-      请勾选需要下载的文件
-      <v-icon @click="emptytree = false">
-        mdi-close
-      </v-icon>
-    </v-btn>
-    <v-sheet class="pa-4 primary lighten-2">
-      <v-text-field v-model="filterText" label="输入关键字进行过滤" dark flat solo-inverted hide-details clearable clear-icon="mdi-close-circle-outline"></v-text-field>
-    </v-sheet>
-    <v-treeview v-loading="loading" selectable v-model="tree" :filter="filter" :search="filterText" :open="initiallyOpen" :items="resources" activatable item-key="address" open-on-click>
-      <template v-slot:prepend="{ item, open }">
-        <v-icon v-if="item.file == 'folder'">
-          {{ open ? 'mdi-folder-open' : 'mdi-folder' }}
-        </v-icon>
-        <v-icon v-else-if="files.hasOwnProperty(item.file)">
-          {{ files[item.file] }}
-        </v-icon>
-        <v-icon v-else>
-          {{ files["unknown"] }}
-        </v-icon>
-      </template>
-      <template v-slot:append="{ item }">
-        <v-btn v-if="item.file != 'folder'" icon @click="handlePreview(item)">
-          <v-icon>
-            mdi-eye
-          </v-icon>
-        </v-btn>
-        <v-btn v-if="item.file != 'folder'" icon @click="handleDownload(item)">
-          <v-icon>
-            mdi-download
-          </v-icon>
-        </v-btn>
-        <span>XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX</span>
-      </template>
-    </v-treeview>
+  <v-content>
+    <v-card max-width="1000">
+      <v-row>
+        <v-col>
+          <v-btn outlined color="blue" v-show="!treeIsEmpty" @click="handleDownloadZip()">
+            下载<v-icon>mdi-download</v-icon>
+          </v-btn>
+        </v-col>
+        <v-col>
+          <v-btn v-show="downloading">
+            正在下载，请稍后...
+            <v-icon @click="downloading = false" color="green">
+              mdi-close
+            </v-icon>
+          </v-btn>
+        </v-col>
+        <v-col>
+          <v-btn outlined color="green" @click="refresh()">
+            刷新<v-icon>mdi-cached</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+      <v-sheet class="pa-4 primary lighten-2">
+        <v-text-field v-model="filterText" label="输入关键字进行过滤" dark flat solo-inverted hide-details clearable></v-text-field>
+      </v-sheet>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+        <v-treeview class="text-left" v-loading="loading" selectable v-model="tree" :filter="filter" :search="filterText" :open="initiallyOpen" :items="resources" activatable item-key="address" open-on-click>
+          <template v-slot:prepend="{ item, open }">
+            <v-icon v-if="item.file == 'folder'">
+              {{ open ? 'mdi-folder-open' : 'mdi-folder' }}
+            </v-icon>
+            <v-icon v-else-if="files.hasOwnProperty(item.file)">
+              {{ files[item.file] }}
+            </v-icon>
+            <v-icon v-else>
+              {{ files["unknown"] }}
+            </v-icon>
+          </template>
+          <template v-slot:append="{ item }">
+            <v-btn v-if="item.file != 'folder'" icon @click="handlePreview(item)">
+              <v-icon>
+                mdi-eye
+              </v-icon>
+            </v-btn>
+            <v-btn v-if="item.file != 'folder'" icon @click="handleDownload(item)">
+              <v-icon>
+                mdi-download
+              </v-icon>
+            </v-btn>
+          </template>
+        </v-treeview>
+        </v-col>
+      </v-row>
+    </v-card>
   </v-content>
   <v-footer>
   </v-footer>
@@ -82,7 +93,6 @@ export default {
       filterText: '',
       loading: true,
       downloading: false,
-      emptytree: false,
     }
   },
   name: 'Resource',
@@ -99,41 +109,28 @@ export default {
     filter() {
       return (item, search, textKey) => item[textKey].indexOf(search) > -1;
     },
+    treeIsEmpty() {
+      return this.tree.length === 0;
+    }
   },
   methods: {
     handleDownloadZip() {
       const files = this.tree;
       console.log(files);
-      if (files.length === 0) {
-        this.emptytree = true;
-      } else {
-        this.downloading = true;
-        this.$http.get("/api/files", {
-          params: {
-            address: files
-          },
-          responseType: 'arraybuffer',
-        }).then(
-          (res) => {
-            const url = window.URL.createObjectURL(new Blob([res.data]));
-            const link = document.createElement('a');
-            link.style.display = 'none';
-            link.href = url;
-            link.setAttribute('download', 'tmp.zip');
-            document.body.appendChild(link);
-            link.click();
-            link.remove();
-            this.downloading = false;
-          },
-          (res) => {
-            this.$notify.error({
-              title: "错误",
-              message: "请重新登录",
-            });
-            this.$router.push('/app/login');
-          }
-        )
-      }
+      this.downloading = true;
+      this.$http.get("/api/files", {
+        params: {
+          address: files
+        },
+        responseType: 'arraybuffer',
+      }).then(
+        (res) => {
+          this.downloadSuccess(res, 'tmp.zip');
+        },
+        (res) => {
+          this.redirectToLogin();
+        }
+      )
     },
     handleNodeClick() {
 
@@ -151,11 +148,7 @@ export default {
           this.loading = false;
         },
         (res) => {
-          this.$notify.error({
-            title: "错误",
-            message: "请重新登录",
-          });
-          this.$router.push('/app/login');
+          this.redirectToLogin();
         }
       )
     },
@@ -170,24 +163,30 @@ export default {
         responseType: 'arraybuffer',
       }).then(
         (res) => {
-          const url = window.URL.createObjectURL(new Blob([res.data]));
-          const link = document.createElement('a');
-          link.style.display = 'none';
-          link.href = url;
-          link.setAttribute('download', file.name);
-          document.body.appendChild(link);
-          link.click();
-          link.remove();
-          this.downloading = false;
+          this.downloadSuccess(res, file.name);
         },
         (res) => {
-          this.$notify.error({
-            title: "错误",
-            message: "请重新登录",
-          });
-          this.$router.push('/app/login');
+          this.redirectToLogin();
         }
       )
+    },
+    downloadSuccess(res, name) {
+      const url = window.URL.createObjectURL(new Blob([res.data]));
+      const link = document.createElement('a');
+      link.style.display = 'none';
+      link.href = url;
+      link.setAttribute('download', name);
+      document.body.appendChild(link);
+      link.click();
+      link.remove();
+      this.downloading = false;
+    },
+    redirectToLogin() {
+      this.$notify.error({
+        title: "错误",
+        message: "请重新登录",
+      });
+      this.$router.push('/app/login');
     },
     handlePreview(file) {
       this.$http.get("/api/file", {
@@ -204,11 +203,7 @@ export default {
           window.open(pdfUrl);
         },
         (res) => {
-          this.$notify.error({
-            title: "错误",
-            message: "请重新登录",
-          });
-          this.$router.push('/app/login');
+          this.redirectToLogin();
         }
       )
     },
