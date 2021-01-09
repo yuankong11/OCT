9c4010b8cfa8df119b41f118eac07a262e16377d
miner <50past12am@gmail.com>
2021-01-09 19:06:16 +0800

    User模型中新增timetable_ics字段存放ics地址

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_09_065528) do
+ActiveRecord::Schema.define(version: 2021_01_09_110424) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_01_09_065528) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "timetable_ics"
   end
 
 end
